51ef69fbf182a11b67613bd246af184e6eb4bd3d
TokamLilian <tokamlilian@gmail.com>
2024-02-02 14:41:52 -0500

    Unit test of moving dat Initial commit

---
+++ TP1/unit_test.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+import Stack.ArrayStack;
+import DoubleStack.ArrayDoubleStack;
+
+public class unit_test {
+    public static void main(String[] args){
+        ArrayStack<String> Nam = new ArrayStack<>();
+        pushNam(Nam);
+        //System.out.println(Nam.toString());
+        System.out.println(Nam.Top());
+    }
+
+    public static void pushNam(ArrayStack<String> stack){
+        // create new scanner
+        Scanner input = new Scanner(System.in);
+        
+        for (int i=5; i>0; i--){
+            //System.out.println("Enter a layer: ");
+            //String layer = input.nextLine();
+            String[] layers = {"Agriculture", "Manufacture", "Academia", "Government", "The King" };
+            String layer = layers [i-1];
+            stack.Push("L"+ i +  ": " +layer );
+        }
+        input.close();
+
+    }
+}
