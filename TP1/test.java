a08e8f7f425f072dae40baf314487f36996161bd
TokamLilian <tokamlilian@gmail.com>
2024-02-02 09:09:54 -0500

    Added tests for push, pop and top for ArrayDoubleStack

--- TP1/test.java
+++ TP1/test.java
@@ -3,6 +3,10 @@ import Stack.ArrayStack;
 import DoubleStack.ArrayDoubleStack;
 
 public class test {
+
+    public static void main(String[] args) {
+        InsertAndPop();
+    }
     
     public static void ArrayStack_Test () {
         ArrayStack newSimpleArray = new ArrayStack(1);
@@ -12,6 +16,25 @@ public class test {
     public static void ArrayDoubleStack_Test () {
         ArrayDoubleStack newDoubleArray = new ArrayDoubleStack(1);
         System.out.println(newDoubleArray);
+    }
+    
+    public static void InsertAndPop(){
+        
+        ArrayDoubleStack newDoubleArray = new ArrayDoubleStack(5); 
+        newDoubleArray.Push(true, 5); 
+        newDoubleArray.Push(false,10); 
+        newDoubleArray.Push(false, 15); 
+        newDoubleArray.Push(true, 11); 
+        newDoubleArray.Push(false, 7); 
+        System.out.println("Popped element from stack2 is " + ": " + newDoubleArray.Pop(false)); 
+        newDoubleArray.Push(true, 40); 
+        System.out.println("Popped element from stack1 is "+ ": " + newDoubleArray.Pop(true)); 
+        
+        System.out.println("Top element from stack2 is "+ ": " + newDoubleArray.Top(false)); 
+        System.out.println("Top element from stack1 is "+ ": " + newDoubleArray.Top(true)); 
+
+        System.out.println("Size of stack1 is "+ ": " + newDoubleArray.Size(false)); 
+        System.out.println("Size of stack2 is "+ ": " + newDoubleArray.Size(true)); 
     }
 
 }
