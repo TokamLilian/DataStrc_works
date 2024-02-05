c075aa25748078c4f3c20dc71261a78bf024a20b
TokamLilian <tokamlilian@gmail.com>
2024-02-04 20:12:52 -0500

    Code cleaned and added javadoc

--- TP1/DuplicateEater/DuplicateEater.java
+++ TP1/DuplicateEater/DuplicateEater.java
@@ -9,13 +9,15 @@ public class DuplicateEater {
         String[] list2 = {"tree", "stack", "stack", "tree" };
         String[] list3 = {"stack", "stck", "stack", "tree" };
         String[] list4 = {"stak", "stck", "sack", "queue" };
-        //ArrayStack<String> stack1 = createArray(list1);
-        //System.out.println(pairDestroyer(stack1));
-        ArrayStack<String> stack2 = createArray(list1);
-        System.out.println(pairDestroyer2(stack2));
+        
+        ArrayStack<String> stack = createArray(list1);
+        System.out.println(pairDestroyer(stack));
 
     }
 
+    /*
+     * Create a stack from the given array of strings and return it. 
+     */
     public static ArrayStack<String> createArray(String[] list){
         ArrayStack<String> stack = new ArrayStack<>();
         
@@ -26,58 +28,24 @@ public class DuplicateEater {
         return stack;
     }
 
-    /*
-     * Checks if two similar words within the sequence meet and eliminates them
+
+    /**
+     * This method pushes all the elements in temp into "stack".
      */
-    public static int pairDestroyer(ArrayStack<String> myStack){
-        ArrayStack<String> temp = new ArrayStack<>();
+    public static ArrayStack<String> emptyTemp(ArrayStack<String> stack, ArrayStack<String> temp){
 
-        boolean duplicatesAllFound = false;
-        String topWord = null;
-
-        int firstTop = 0;
-        int secondTop = 0;
-
-        while (firstTop < myStack.size()) {
-            // Get the current word on top of the stack
-            topWord = myStack.Pop();
-
-            while (secondTop != myStack.size()) { //iterate on the elements of the stack
-                
-                // Now get the "next" word
-                String nextWord = myStack.Pop();
-
-                // If they are a match, remove the duplicate
-                if (topWord == nextWord) {
-                    if (myStack.size() < 2 && temp.size() < 2) duplicatesAllFound = true;
-                    // maintain the removed the duplicate from the stack
-                    if (temp.size() > 0 && myStack.size() > 0) { //!
-                        while (temp.size() != 0){
-                            myStack.Push(temp.Pop());
-                        }
-                    }
-                    if (myStack.size() < 2 || duplicatesAllFound == true) 
-                        return myStack.size();
-                    else break;
-                } else {                        // Otherwise just push the original word
-                    temp.Push(nextWord);      // Onto the temp stack
-                }
-
-                // Update our second-top variable for the next iteration
-                //secondTop ++;
-            }
-            // empty temp except the previous topWord from myStack
-            while (temp.size() != firstTop){
-                myStack.Push(temp.Pop());
-            }
-            temp.Push(topWord);
-            // Update our first-top variable for the next iteration
-            firstTop ++;
-
+        while (!temp.isEmpty()){
+            stack.Push(temp.Pop());
         }
-        return secondTop; //
+
+        return stack;
     }
 
+    
+    /**
+     * This method compares elements at top of the "stack". If they are equal, it pops both  
+     * from "stack". Otherwise, it pushes the top of "stack" to "temp" and pops it from "stack".
+     */
     public static ArrayStack<String> destroyer(ArrayStack<String> reducedStack){
         ArrayStack<String> temp = new ArrayStack<>();
         String topWord = reducedStack.Pop();
@@ -86,29 +54,25 @@ public class DuplicateEater {
             String nextWord = reducedStack.Pop();
             
             if (topWord == nextWord){
-                while (!temp.isEmpty()){
-                    reducedStack.Push(temp.Pop());
-                }
+                reducedStack = emptyTemp(reducedStack, temp);
                 return reducedStack;
+
             }else{
                 temp.Push(nextWord);
             }
         }
+        
+        reducedStack = emptyTemp(reducedStack, temp);
 
-        if (temp.size() > 0){
-            while (!temp.isEmpty()){
-                reducedStack.Push(temp.Pop());
-            }
+        return reducedStack;
 
-            return reducedStack;
-
-        } else{ 
-            return reducedStack;
-        }
     }
 
 
-    public static int pairDestroyer2(ArrayStack<String> myStack){
+    /*
+     * Checks if two similar words within the sequence meet and eliminates them
+     */
+    public static int pairDestroyer(ArrayStack<String> myStack){
         ArrayStack<String> temp = new ArrayStack<>();
 
         while (myStack.size() > 0){
@@ -125,11 +89,7 @@ public class DuplicateEater {
             
         }
 
-        if (!temp.isEmpty()){
-            while (!temp.isEmpty()){
-                myStack.Push(temp.Pop());
-            }
-        }
+        myStack = emptyTemp(myStack, temp);
         return myStack.size();
 
     }
