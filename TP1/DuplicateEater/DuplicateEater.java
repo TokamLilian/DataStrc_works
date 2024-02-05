4eeba09703ecf8363f7df5904dd92ec28895ca41
TokamLilian <tokamlilian@gmail.com>
2024-02-05 10:23:57 -0500

    Moved main method to test file

--- TP1/DuplicateEater/DuplicateEater.java
+++ TP1/DuplicateEater/DuplicateEater.java
@@ -3,31 +3,6 @@ package DuplicateEater;
 import Stack.ArrayStack;
 
 public class DuplicateEater {
-    public static void main(String[] args) {
-        
-        String[] list1 = {"stack", "list", "list", "queue", "tree" };
-        String[] list2 = {"tree", "stack", "stack", "tree" };
-        String[] list3 = {"stack", "stck", "stack", "tree" };
-        String[] list4 = {"stak", "stck", "sack", "queue" };
-        
-        ArrayStack<String> stack = createArray(list1);
-        System.out.println(pairDestroyer(stack));
-
-    }
-
-    /*
-     * Create a stack from the given array of strings and return it. 
-     */
-    public static ArrayStack<String> createArray(String[] list){
-        ArrayStack<String> stack = new ArrayStack<>();
-        
-        for (int i=0; i<list.length; i++){            
-            String element = list[i];
-            stack.Push(element);
-        }
-        return stack;
-    }
-
 
     /**
      * This method pushes all the elements in temp into "stack".
