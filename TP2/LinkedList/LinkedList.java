0d1b3a4714925c76e900cb9894ebca0670a94f05
Tokam Lilian <79397981+TokamLilian@users.noreply.github.com>
2024-02-14 08:51:10 -0500

    Finished coding the iterative functions

--- TP2/LinkedList/LinkedList.java
+++ TP2/LinkedList/LinkedList.java
@@ -1,6 +1,6 @@
 package LinkedList;
 
-public class LinkedList<Integer> implements List {   
+public class LinkedList implements List<Integer> {   
     private Node tail; // tail of list  
     private Node head; // tale of list  
     
@@ -10,20 +10,7 @@ public class LinkedList<Integer> implements List {
         this.head = new Node(0);
     }  
 
-  
-    /**  Returns the number of elements in the list. */
-    @Override
-    public int size() {
-        Node current = head;        
-        int count = 0;  
-        while (current != null) {             
-            count++;                           
-            current = current.getNext();
-        }                            
-        return count;  
-    } 
 
-      
     /** Inserts element at a specific position of the list. */
     public void add(int index, int element) throws IndexOutOfBoundsException{ 
         if (index < 0 || index > size()) throw new IndexOutOfBoundsException();    
@@ -37,7 +24,7 @@ public class LinkedList<Integer> implements List {
             //head.setPrevious(node); 
             head = node;                           // And set head to be our new node.
 
-        }else if (index == size() ){ add(element);        // Or if we want to append to the end...
+        }else if (index == size() ){ addLast(element);        // Or if we want to append to the end...
 
         } else {                                   // Otherwise, find the correct position and insert there.
             while (position++ < index) {           // Move through the list until we reach the right position.
@@ -52,23 +39,6 @@ public class LinkedList<Integer> implements List {
                 currentNode.setNext(node);      // Set next pointer of old node to new node.
             }
         } 
-    }
-
-
-    /** Add item at tail (end) of linkedlist (append).*/
-    public void add(int element){
-        Node node =  new Node(element);
-        if (tail.getData() == 0) {                      // If the list is empty, make this node the tail.
-            tail = node;                                   // Set the tail of the list to be the new node.
-            head = node;                                   // Also set it as the head for simplicity.
-            //tail.setPrevious(head);
-            head.setNext(node);
-        }else{
-            //node.setPrevious(tail);                            // Point the new node's previous to the current tail.
-            tail.setNext(node);                                // Set next pointer of tail to point to new node.
-
-            tail = node;                                       // Update the tail of the List
-        }
     }
 
 
@@ -96,7 +66,7 @@ public class LinkedList<Integer> implements List {
     }
 
 
-    /** Return the element stored at the given index
+    /** Return the element at the given index
      */
     public int get(int index){
         if (index < 0 || index >= size()) throw new IndexOutOfBoundsException("Index out of range: "+index+" Size:"+size());
@@ -128,9 +98,8 @@ public class LinkedList<Integer> implements List {
         
     }
 
-
-    @Override
-    public String  toString(){
+    // @Overide
+    public String toString(){
         StringBuilder sb = new StringBuilder("[");
         Node currentNode = head;
 
@@ -145,58 +114,97 @@ public class LinkedList<Integer> implements List {
     }
 
 
+    /**  Returns the number of elements in the list. */
+    @Override
+    public int size() {
+        Node current = head;        
+        int count = 0;  
+        while (current != null) {             
+            count++;                           
+            current = current.getNext();
+        }                            
+        return count;  
+    } 
+
+
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+        return (head == tail && head.getData() == 0);
     }
 
 
     @Override
     public int first() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'first'");
+        return head.getData();
     }
 
 
     @Override
     public int last() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'last'");
+        return tail.getData();
     }
 
 
+    /** Add item at head (begining) of linkedlist.*/
     @Override
     public void addFirst(int value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addFirst'");
+        Node node =  new Node(value);
+        if (head.getData() == 0) {                         // If the list is empty, make this node the head.
+            head = node;                                   // Set it as the head for simplicity.
+            tail = node;                                   // Also set the tail of the list to be the new node.
+        }else{
+            node.setNext(head);                            // Set next pointer of current node to point to previous head.
+            head = node;                                   // Update the head of the List
+        }
     }
 
 
+    /** Add item at tail (end) of linkedlist (append).*/
     @Override
     public void addLast(int value) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addLast'");
+        Node node =  new Node(value);
+        if (tail.getData() == 0) {                         // If the list is empty, make this node the tail.
+            tail = node;                                   // Set the tail of the list to be the new node.
+            head = node;                                   // Also set it as the head for simplicity.
+            head.setNext(node);
+        }else{
+            tail.setNext(node);                            // Set next pointer of tail to point to new node.
+            tail = node;                                   // Update the tail of the List
+        }
     }
 
 
     @Override
     public void removeFirst() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeFirst'");
+        Node temp = head.getNext();
+        head.setNext(null);
+
+        head = temp;
+        
     }
 
 
     @Override
     public void removeLast() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeLast'");
+        int size = size();
+        Node retval = head;                      // Save the value that will be returned
+        Node previous = head;                    // Points to the node before the one we're looking for.
+        int pos = 0;                             // Current position in the list.
+
+        while (pos++ < size) {                   // Loop through the list until we find the right spot.
+            previous = retval;                   // Move the "previous" pointer along.
+            retval = retval.getNext();           // Move "retval" to point to the next node.
+        }
+
+        previous.setNext(null);
+        tail = previous;                         // The removed node was the last one hence update the tail.
     }
     
     
     /** Return the n-th element from the end backwards */
-    public int returnNlast(int nLast){
-        int temp = 0;
+    public int returnNLast(int nLast){
+        int size = size();
+        int temp = get(size-nLast);
         
         return temp;
     }
@@ -208,13 +216,21 @@ public class LinkedList<Integer> implements List {
      * @return true or false
      */
     public boolean checkInList(int value){
-        return true;
+        Node gotNode = head;
+    
+        while (gotNode != null){
+            if (gotNode.getData() == value) return true;
+            gotNode = gotNode.getNext();
+        }
+
+        return false;
     }
 
 
     /** Recursive method to remove an element present in the list */
     public void removeValue(int value){
-
+        int retIndex = getId(value);
+        remove(retIndex);
     }
 
 
@@ -223,9 +239,18 @@ public class LinkedList<Integer> implements List {
      * @return
      */
     public int maxValue(){
-        int temp = 0;
+        int maxVal = 0;
+        Node currNode = head;
+        maxVal = currNode.getData();
 
-        return temp;
+        while (currNode.getNext() != null){
+            currNode = currNode.getNext();
+            int comparedValue = currNode.getData();
+
+            if (comparedValue > maxVal) maxVal = comparedValue;
+        }
+
+        return maxVal;
 
     }
 
@@ -243,5 +268,5 @@ public class LinkedList<Integer> implements List {
     public void addInOrder(int value){
 
     }
-
+    
 }
