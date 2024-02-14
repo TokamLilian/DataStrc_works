fa2b87862d8c4ecb1e3eed21eb6a1a9e00c96b54
Tokam Lilian <79397981+TokamLilian@users.noreply.github.com>
2024-02-14 07:47:47 -0500

    Only methods asked by the assignment are in the super class

--- TP2/LinkedList/List.java
+++ TP2/LinkedList/List.java
@@ -4,13 +4,13 @@ package LinkedList;
  * List interface with methods implemented by Lists
  */
 public interface List<Integer> {
-    void add(int index, int element);
-    void add(int element);
+    //void add(int index, int element);
+    //void add(int element);
 
-    void remove(int index);
+    //void remove(int index);
 
-    int get(int index);
-    int getId(int element);
+    //int get(int index);
+    //int getId(int element);
 
     int size();
     boolean isEmpty();
@@ -18,8 +18,10 @@ public interface List<Integer> {
     int first();
     int last();
 
-    void addFirst();
+    void addFirst(int value);
+    void addLast(int value);
+
     void removeFirst();
-    void addLast();
+    void removeLast();
 
 }
