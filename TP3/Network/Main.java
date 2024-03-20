953524fa4873d7875972cc27e5f4bbcf16b85e40
Tokam Lilian <79397981+TokamLilian@users.noreply.github.com>
2024-03-19 20:14:56 -0400

    Initial commit

---
+++ TP3/Network/Main.java
@@ -0,0 +1,8 @@
+package Network;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // Server server = new Server("127.0.0.1", 8090);
+        // client c = new client();
+    }
+}
