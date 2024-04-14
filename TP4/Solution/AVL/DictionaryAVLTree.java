6e79f183a31b8d23342bedc0ca6fba8ef23ce350
Tokam Lilian <79397981+TokamLilian@users.noreply.github.com>
2024-04-14 17:26:00 -0400

    Finished coding the AVL tree.

--- TP4/Solution/AVL/DictionaryAVLTree.java
+++ TP4/Solution/AVL/DictionaryAVLTree.java
@@ -13,7 +13,7 @@ class AVLNode {
 
 
 
-public class DictionaryAVLTree {
+class DictionaryAVLTree {
     private AVLNode root;
 
     private int height(AVLNode N) {
@@ -21,9 +21,97 @@ public class DictionaryAVLTree {
         return N.height;
     }
 
-    /* A */
+    int max(int a, int b) { 
+        return (a > b) ? a : b; 
+    } 
+
+    AVLNode rightRotate(AVLNode y)  
+    {  
+        AVLNode x = y.left;  
+        AVLNode T2 = x.right;  
+  
+        // Perform rotation  
+        x.right = y;  
+        y.left = T2;  
+  
+        // Update heights  
+        y.height = max(height(y.left), height(y.right)) + 1;  
+        x.height = max(height(x.left), height(x.right)) + 1;  
+  
+        // Return new root  
+        return x;  
+    }  
+  
+    // A utility function to left 
+    // rotate subtree rooted with x  
+    // See the diagram given above.  
+    AVLNode leftRotate(AVLNode x)  
+    {  
+        AVLNode y = x.right;  
+        AVLNode T2 = y.left;  
+  
+        // Perform rotation  
+        y.left = x;  
+        x.right = T2;  
+  
+        // Update heights  
+        x.height = max(height(x.left), height(x.right)) + 1;  
+        y.height = max(height(y.left), height(y.right)) + 1;  
+  
+        // Return new root  
+        return y;  
+    }  
+
+    int getBalance(AVLNode N) 
+    {  
+        if (N == null)  
+            return 0;  
+  
+        return height(N.left) - height(N.right);  
+    }  
+
+    /* Insert a new word and it's meaning to the dictionary and maintain order */
     private AVLNode insert(AVLNode node, String word, String meaning) {
+        /* 1. Perform the normal BST insertion */
+        if(node == null) return (new AVLNode(word, meaning));
+
+        if (word.compareTo(node.word) < 0) 
+            node.left = insert(node.left, word, meaning);  
+        else if (word.compareTo(node.word) > 0)
+            node.right = insert(node.right, word, meaning);  
+        else // Duplicate words not allowed  
+            return node;  
+  
+        /* 2. Update height of this ancestor node */
+        node.height = 1 + max(height(node.left), height(node.right));  
+  
+        /* 3. Get the balance factor of this ancestor node to check whether this node became unbalanced */
+        int balance = getBalance(node);  
+  
+        // If this node becomes unbalanced
+
+        // Left Left Case  
+        if (balance > 1 && word.compareTo(node.left.word) < 0)  
+            return rightRotate(node);  
+  
+        // Right Right Case  
+        if (balance < -1 && word.compareTo(node.right.word) > 0)  
+            return leftRotate(node);  
+  
+        // Left Right Case  
+        if (balance > 1 && word.compareTo(node.left.word) > 0){
+            node.left = leftRotate(node.left);  
+            return rightRotate(node);
+        }
+
+        // Right Left Case  
+        if (balance < -1 && word.compareTo(node.right.word) < 0){  
+            node.right = rightRotate(node.right);  
+            return leftRotate(node);  
+        }  
    
+        /* return the (unchanged) node pointer */
+        return node;
     }
 
 
@@ -31,9 +119,79 @@ public class DictionaryAVLTree {
         root = insert(root, word, meaning);
     }
     
+    AVLNode minValueNode(AVLNode node) {
+        AVLNode current = node;
 
+        while (current.left != null) {
+            current = current.left;
+        }
+
+        return current;
+    }
+
+    /* Remove a word from the dictionary */
     private AVLNode delete(AVLNode root, String word) {
-       
+        if (root == null) {
+            return root;
+        }
+
+        if (word.compareTo(root.word) < 0) {
+            root.left = delete(root.left, word);
+        } else if (word.compareTo(root.word) > 0) {
+            root.right = delete(root.right, word);
+        } else {
+            if (root.left == null || root.right == null) {
+                AVLNode temp = null;
+                if (temp == root.left) {
+                    temp = root.right;
+                } else {
+                    temp = root.left;
+                }
+
+                if (temp == null) {
+                    temp = root;
+                    root = null;
+                } else {
+                    root = temp;
+                }
+            } else {
+                AVLNode temp = minValueNode(root.right);
+                root.word = temp.word;
+                root.right = delete(root.right, temp.word);
+            }
+        }
+
+        if (root == null) {
+            return root;
+        }
+
+        root.height = Math.max(height(root.left), height(root.right)) + 1;
+
+        int balance = getBalance(root);
+
+        // Left Left Case
+        if (balance > 1 && getBalance(root.left) >= 0) {
+            return rightRotate(root);
+        }
+
+        // Left Right Case
+        if (balance > 1 && getBalance(root.left) < 0) {
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+
+        // Right Right Case
+        if (balance < -1 && getBalance(root.right) <= 0) {
+            return leftRotate(root);
+        }
+
+        // Right Left Case
+        if (balance < -1 && getBalance(root.right) > 0) {
+            root.right = rightRotate(root.right);
+            return leftRotate(root);
+        }
+
+        return root;
     }
     
 
@@ -41,9 +199,21 @@ public class DictionaryAVLTree {
         root = delete(root, word);
     }
     
-
+    /* Look for the definition of a given word in the dictionary */
     public String search(AVLNode root, String word) {
-    
+        if (root == null) {
+            return null;
+        }
+        
+        if(root.word.equals(word)) {
+            return root.meaning;
+        }
+
+        if (word.compareTo(root.word) < 0) {
+            return search(root.left, word);
+        } else {
+            return search(root.right, word);
+        }
     }
     
     public String search(String word) {
@@ -63,7 +233,9 @@ public class DictionaryAVLTree {
     public void displayDictionary() {
         inOrderTraversal(root);
     }
+}
 
+class Main{
 
     public static void main(String[] args) {
         DictionaryAVLTree dictionary = new DictionaryAVLTree();
